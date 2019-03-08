a587f2d4bd4bb4d6f65d81d72fec73f96a872850
Artyom Bondartsov <erithion@gmail.com>
2019-03-08 12:30:29 +0200

    gitrev

--- src/Home.hs
+++ src/Home.hs
@@ -4,10 +4,12 @@ module Home where
 
 import Foundation
 import Yesod.Core
+import Smartbook
 
 getHomeR :: Handler Html
 getHomeR = defaultLayout $ do
     setTitle "Smartbook service"
+    let rev = libraryGitInfo
     [whamlet|
-        <p> Smartbook service Alpha
+        <p> Smartbook service Alpha<br> <p> Smartbook library version: #{rev}
     |]
