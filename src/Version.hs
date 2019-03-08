45d407b062da26c97f38f1387766f903bba3e9e5
Artyom Bondartsov <erithion@gmail.com>
2019-03-08 22:10:29 +0200

    version for git hook

--- src/Version.hs
+++ src/Version.hs
@@ -11,7 +11,7 @@ getVersionR :: Handler Html
 getVersionR = defaultLayout $ do
     setTitle "Smartbook service"
     hash :: String <- liftIO $ 
-              catchIOError (read <$> getEnv "APP_GIT_HASH")
+              catchIOError (getEnv "APP_GIT_HASH")
                            (\e -> if isDoesNotExistError e then return "APP_GIT_HASH has not been set upon the last git push" else ioError e)  
     [whamlet|
         <p> Service version: #{hash}
